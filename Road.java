d535ad13800204ced413cd85b3c79b9aeed04203
Jonah Douglas <douglaj8@wwu.edu>
2021-06-07 10:48:27 -0700

    created constructor for Road object

--- Road.java
+++ Road.java
@@ -2,4 +2,10 @@ public class Road {
     String roadName;
     int roadAppearanceTally;            //how often this given road has packages ordered to it (used for optimizing 2d roadmap)
     int roadZipCode;                    //if 00000, road spans multiple zip codes
+
+    public Road(String roadname, int roadzipcode) {
+        roadName = roadname;
+        roadAppearanceTally = 0;
+        roadZipCode = roadzipcode;
+    }
 }
